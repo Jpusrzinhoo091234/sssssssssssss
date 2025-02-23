94357af8a19dc458de3604119b8167027a8c25d8
Jpusrzinhoo091234 <154452487+Jpusrzinhoo091234@users.noreply.github.com>
2025-02-22 23:00:31 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4f46e5',
+        secondary: '#f43f5e',
+        dark: '#0f172a',
+        light: '#f8fafc',
+        element: '#1e293b',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        heading: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
